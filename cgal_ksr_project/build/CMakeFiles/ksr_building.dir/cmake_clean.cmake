70c51cdc8e59fe022359389365f573d1bb10f55c
Ehs9449 <ehsanaghaebrahimi@gmail.com>
2026-02-06 18:30:14 -0600

    Kinetic shape reconstruction

---
+++ cgal_ksr_project/build/CMakeFiles/ksr_building.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ksr_building.dir/ksr_building.cpp.o"
+  "CMakeFiles/ksr_building.dir/ksr_building.cpp.o.d"
+  "ksr_building"
+  "ksr_building.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ksr_building.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
